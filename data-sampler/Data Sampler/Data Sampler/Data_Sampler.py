47376848632eb9e7690bec76c14c981cfb18df8b
Charith Perera <ngcharithperera@gmail.com>
2018-07-13 15:12:04 +0100

    Moved to ws4py

--- data-sampler/Data Sampler/Data Sampler/Data_Sampler.py
+++ data-sampler/Data Sampler/Data Sampler/Data_Sampler.py
@@ -1,35 +1,74 @@
-import websocket
-try:
-    import thread
-except ImportError:
-    import _thread as thread
-import time
+from ws4py.client.threadedclient import WebSocketClient
+import json
 
-def on_message(ws, message):
-    print(message)
+class DummyClient(WebSocketClient):
+    def opened(self):
+        def data_provider():
+            for i in range(1, 200, 25):
+                yield "#" * i
 
-def on_error(ws, error):
-    print(error)
+        self.send(data_provider())
 
-def on_close(ws):
-    print("### closed ###")
+        for i in range(0, 200, 25):
+            print(i)
+            self.send("*" * i)
 
-def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
+    def closed(self, code, reason=None):
+        print("Closed down"), code, reason
+
+    def received_message(self, m):
+        json_string=m.data
+        parsed_json = json.loads(json_string)
+        print(m)
+        print(parsed_json)
+        if len(m) == 175:
+            self.close(reason='Bye bye')
+
+if __name__ == '__main__':
+    try:
+        ws = DummyClient('wss://api.usb.urbanobservatory.ac.uk/stream', protocols=['http-only', 'chat'])
+        ws.connect()
+        ws.run_forever()
+    except KeyboardInterrupt:
         ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
+
+#import websocket
+#import json
+
+#try:
+#    import thread
+#except ImportError:
+#    import _thread as thread
+#import time
+
+#def on_message(ws, message):
+#    json_string=message
+#    parsed_json = json.loads(json_string)
+#    print(message)
+    
+
+#def on_error(ws, error):
+#    print(error)
+
+#def on_close(ws):
+#    print("### closed ###")
+
+#def on_open(ws):
+#    def run(*args):
+#        for i in range(3):
+#            time.sleep(1)
+#            ws.send("Hello %d" % i)
+#        time.sleep(1)
+#        ws.close()
+#        print("thread terminating...")
+#    thread.start_new_thread(run, ())
 
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://api.usb.urbanobservatory.ac.uk/stream",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+#if __name__ == "__main__":
+#    websocket.enableTrace(True)
+#    ws = websocket.WebSocketApp("wss://api.usb.urbanobservatory.ac.uk/stream",
+#                              on_message = on_message,
+#                              on_error = on_error,
+#                              on_close = on_close)
+#    ws.on_open = on_open
+#    ws.run_forever()
